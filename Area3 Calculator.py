8b6421b68051d15454d80f9b6dacdb0039c3de64
SpeedyDude <thatonedeveloper0@gmail.com>
2024-10-04 13:56:06 +0200

    Add files via upload

---
+++ Area3 Calculator.py
@@ -0,0 +1,15 @@
+import time
+
+def calculateArea3(x,y,z):
+   print("\n")
+   print("Area3:",x*y*z,"\n")
+   print("Circumference:",x+y+z,"\n")
+   input("Press enter to exit program.")
+def start_question():
+    x = input("First Number: ")
+    y = input("Second Number: ")
+    z = input("Third Number: ")
+    time.sleep(1)
+    calculateArea3(int(x),int(y),int(z))
+
+start_question()
